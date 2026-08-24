9554c06198bb11d622b6ce96646e83945e12f851
rajrai <rajrai913197@gmail.com>
2026-08-25 01:04:06 +0530

    do animation on nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,12 +21,12 @@ function Navbar() {
       {
         show && (
           <div className="navLinks-home">
-          <Link to="/home">Home</Link>
-          <Link to="/findmatches">Find Matches</Link>
-          <Link to="/interests">Interests</Link>
-          <Link to="/messages">Messages</Link>
-          <Link to="/notification">Notification</Link>
-           <Link to="/myprofile" className="icon-profile-new"><i class="fa-solid fa-circle-user"></i></Link>
+          <Link to="/home" className="bugerLinks">Home</Link>
+          <Link to="/findmatches" className="bugerLinks">Find Matches</Link>
+          <Link to="/interests" className="bugerLinks">Interests</Link>
+          <Link to="/messages" className="bugerLinks" >Messages</Link>
+          <Link to="/notification" id="bugerLinksStyle">Notification</Link>
+           <Link to="/myprofile" className="icon-profile-new" id="bugerLinksStyle2">My Profile</Link>
         </div>
         )
       }
